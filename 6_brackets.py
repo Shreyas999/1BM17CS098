c337486df5deee2b954edd3c2571a1b146295ca8
Shreyas999 <53894147+Shreyas999@users.noreply.github.com>
2019-10-10 11:45:09 +0530

    Update 6_brackets.py

--- 6_brackets.py
+++ 6_brackets.py
@@ -1,24 +1,29 @@
-a=['*']
-b=list(input("Enter the string:"))
-a.append(b[0])
-ai=1
-for  i in range(1,len(b)):
-    if b[i]=='[' or b[i]=='(' or b[i]=='{':
-        a.append(b[i])
-        ai+=1
-    if (b[i]==']'):
-        if a[ai]=='[':
-            a.pop()
-            ai-=1
-    if (b[i]==')'):
-        if a[ai]=='(':
-            a.pop()
-            ai-=1
-    if (b[i]=='}'):
-        if a[ai]=='{':
-            a.pop()
-            ai-=1
-if a==['*']:
-    print("Valid")
-else:
-    print("Invalid")
+class brackets:
+    def check(self):
+        a=['*']
+        b=input("Enter the string:")
+        a.append(b[0])
+        ai=1
+        for  i in range(1,len(b)):
+            if b[i]=='[' or b[i]=='(' or b[i]=='{':
+                a.append(b[i])
+                ai+=1
+            if (b[i]==']'):
+                if a[ai]=='[':
+                    a.pop()
+                    ai-=1
+            if (b[i]==')'):
+                if a[ai]=='(':
+                    a.pop()
+                    ai-=1
+            if (b[i]=='}'):
+                if a[ai]=='{':
+                    a.pop()
+                    ai-=1
+        if a==['*']:
+            print("Valid")
+        else:
+            print("Invalid")
+
+b1=brackets()
+b1.check()
